47818030ee92532558be1eb33f52becec705c070
Mitlyy <d.mitrii@outlook.com>
2025-06-19 14:44:03 +0300

    link fix

--- download_extract_chekpoints.sh
+++ download_extract_chekpoints.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="https://drive.google.com/drive/folders/18vWPf63O8CPGyS8NZQvAlYMSz6N7ewlf?usp=sharing/"
+URL="https://drive.google.com/file/d/1byGTGLx1jJz-VX7R6jZZyexBzJFsgrRm/view?usp=drive_link"
 
 ZIPFILE="checkpoints.zip"
 wget "$URL" -O "$ZIPFILE"
